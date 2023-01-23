41636e6f927fb0283cb539febac031866f7114d0
Alessandro Ganino s304890 <73252380+aleganino@users.noreply.github.com>
2023-01-23 18:32:31 +0100

    Added link from "Choose Table" to others screen

---
+++ StudyRoom-RPG/Assets/Code/ChooseTableScript.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ChooseTableScript : MonoBehaviour
+{
+
+    public Button next_b;
+    public VerticalLayoutGroup selection_group;
+    List<Button> subjects = new List<Button>();
+    static string table_name;
+    // Start is called before the first frame update
+    void Start()
+    {
+        next_b.interactable = false;
+        for (int i=0; i<selection_group.transform.childCount; i++) {
+            Button button = selection_group.transform.GetChild(i).gameObject.GetComponent<Button>();
+            subjects.Add(button);
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void onTableSelection() {
+        GameObject selected_object = EventSystem.current.currentSelectedGameObject;
+        if (selected_object!=null && subjects.Contains(selected_object.GetComponent<Button>())) {
+            table_name = selected_object.transform.GetChild(0).GetComponent<Text>().text;
+            next_b.interactable = true;
+        }
+        else {
+            next_b.interactable = false;
+        }
+    }
+}
