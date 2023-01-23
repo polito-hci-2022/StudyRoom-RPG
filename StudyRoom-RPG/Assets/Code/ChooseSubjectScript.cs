7d4ae52e34465960ae2e073b786f3673b601895c
Alessandro Ganino s304890 <73252380+aleganino@users.noreply.github.com>
2023-01-23 13:13:08 +0100

    Added Subject selection

--- StudyRoom-RPG/Assets/Code/ChooseSubjectScript.cs
+++ StudyRoom-RPG/Assets/Code/ChooseSubjectScript.cs
@@ -2,24 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class ChooseSubjectScript : MonoBehaviour
 {
 
-    Button next_b;
-    //GameObject selection;
-
+    public Button next_b;
+    public VerticalLayoutGroup selection_group;
+    List<Button> subjects = new List<Button>();
     // Start is called before the first frame update
     void Start()
     {
-        next_b = gameObject.transform.GetChild(2).gameObject.GetComponent<Button>();
         next_b.interactable = false;
+        for (int i=0; i<selection_group.transform.childCount; i++) {
+            Button button = selection_group.transform.GetChild(i).gameObject.GetComponent<Button>();
+            subjects.Add(button);
+        }
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        //selection = gameObject.transform.GetChild(3).gameObject.transform.GetChild(0);
+
     }
+
+    public void onSubjectSelection() {
+        GameObject selected_object = EventSystem.current.currentSelectedGameObject;
+        if (selected_object!=null && subjects.Contains(selected_object.GetComponent<Button>())) {
+            next_b.interactable = true;
+        }
+        else {
+            next_b.interactable = false;
+        }
+    }
+
 }
