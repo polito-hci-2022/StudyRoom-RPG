f021171c31f0cfbbe4f59ad661bb53e056f56966
Alessandro Ganino s304890 <73252380+aleganino@users.noreply.github.com>
2023-01-23 18:13:30 +0100

    Added static variable "subject_name"
    Used to share the chosen subject between scenes

--- StudyRoom-RPG/Assets/Code/ChooseSubjectScript.cs
+++ StudyRoom-RPG/Assets/Code/ChooseSubjectScript.cs
@@ -10,6 +10,7 @@ public class ChooseSubjectScript : MonoBehaviour
     public Button next_b;
     public VerticalLayoutGroup selection_group;
     List<Button> subjects = new List<Button>();
+    static string subject_name;
     // Start is called before the first frame update
     void Start()
     {
@@ -30,6 +31,7 @@ public class ChooseSubjectScript : MonoBehaviour
     public void onSubjectSelection() {
         GameObject selected_object = EventSystem.current.currentSelectedGameObject;
         if (selected_object!=null && subjects.Contains(selected_object.GetComponent<Button>())) {
+            subject_name = selected_object.transform.GetChild(0).GetComponent<Text>().text;
             next_b.interactable = true;
         }
         else {
