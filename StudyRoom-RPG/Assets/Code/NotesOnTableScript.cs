4902c136a37d23e19b33d681ec70506593ec78e5
Alessandro Ganino s304890 <73252380+aleganino@users.noreply.github.com>
2023-01-28 19:10:47 +0100

    First interaction between your notes and table notes

--- StudyRoom-RPG/Assets/Code/NotesOnTableScript.cs
+++ StudyRoom-RPG/Assets/Code/NotesOnTableScript.cs
@@ -7,10 +7,17 @@ using UnityEngine.EventSystems;
 public class NotesOnTableScript : MonoBehaviour
 {
     List<Document> document_list;
+    public GameObject note1, note2, note3, note4;
+
     // Start is called before the first frame update
     void OnEnable()
     {
+        GameObject[] notes = {note1, note2, note3, note4};
         document_list = DocumentList.instance.GetDocumentsOnTable();
+        for (int i=0; i<document_list.Count; i++) {
+            notes[i].SetActive(true);
+        }
+
     }
 
     // Update is called once per frame
