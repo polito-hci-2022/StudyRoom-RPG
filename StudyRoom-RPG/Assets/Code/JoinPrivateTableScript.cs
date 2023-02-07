ed7aabcc1b81778b85276ad3d784e591d65c1382
Alessandro Ganino s304890 <73252380+aleganino@users.noreply.github.com>
2023-02-07 10:05:12 +0100

    Added code error prevention

--- StudyRoom-RPG/Assets/Code/JoinPrivateTableScript.cs
+++ StudyRoom-RPG/Assets/Code/JoinPrivateTableScript.cs
@@ -8,11 +8,14 @@ public class JoinPrivateTableScript : MonoBehaviour
 {
     public Button next_b;
     public InputField input_code;
+    public Text warning;
+
     string code;
+    int i;
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
@@ -20,7 +23,18 @@ public class JoinPrivateTableScript : MonoBehaviour
     {
         //code is child 2 because a Caret is automaticly added when the InputField is used
         code = input_code.transform.GetChild(2).gameObject.GetComponent<Text>().text;
-        next_b.interactable = (code != "");
+        if (code.Length == 6 && int.TryParse(code, out i)) {
+            warning.gameObject.SetActive(false);
+            next_b.interactable = true;
+        }
+        else if (code.Length == 0) {
+            warning.gameObject.SetActive(false);
+            next_b.interactable = false;
+        }
+        else {
+            warning.gameObject.SetActive(true);
+            next_b.interactable = false;
+        } 
     }
 
     public void OnTableConfirmation() {
